123aa4ab4de6376e4c84aec069d54636c3c99d72
Luke Humphrey <luke.humphrey@ukaea.uk>
2023-12-22 15:04:23 +0000

    📝 Update example scripts

--- examples/example_1_simple_monoblock.py
+++ examples/example_1_simple_monoblock.py
@@ -1,37 +1,40 @@
+"""
+SLEDO Simple Monoblock Example
+
+Example input file for running Bayesian optimisation of a simplified divertor
+monoblock.
+
+(c) Copyright UKAEA 2023.
+"""
+
 from sledo.optimiser import Optimiser
 from sledo.simulation import ThermoMechSimulation
+from ray import tune
 
-DATA_DIR = "./results/example_1"
+DATA_DIR = "./results/example_simple_monoblock"
 
-search_space = (
-    {
-        "name": "Armour height",
-        "type": "range",
-        "hit_name": "monoBArmHeight",
-        "hit_block": "",
-        "bounds": [1e-3, 20e-3],
-    },
-    {
-        "name": "Block width",
-        "type": "range",
-        "hit_name": "monoBWidth",
-        "hit_block": "",
-        "bounds": [17e-3, 34e-3],
-    },
-)
+if __name__ == '__main__':
 
-opt = Optimiser(
-    name="simple_monoblock",
-    search_space=search_space,
-    simulation_class=ThermoMechSimulation,
-    data_dir=DATA_DIR,
-)
+    # Define a search space according to the Ray Tune API.
+    # Documentation here:
+    # https://docs.ray.io/en/latest/tune/api/search_space.html
+    search_space = {
+        "Armour height": tune.uniform(1e-3, 20e-3),
+        "Block width": tune.uniform(17e-3, 34e-3),
+    }
 
-opt.load_input_file("./input_files/simple_monoblock.i")
-opt.run_optimisation_loop(
-    objective_name="stress",
-    max_iter=20,
-    minimise=True,
-)
+    # Instantiate the Sledo Optimiser class.
+    opt = Optimiser(
+        name="simple_monoblock",
+        search_space=search_space,
+        simulation_class=ThermoMechSimulation,
+        data_dir=DATA_DIR,
+    )
 
-opt.pickle()
+    opt.run_optimisation_loop(
+        objective_name="stress",
+        max_iter=20,
+        minimise=True,
+    )
+
+    opt.pickle()
