a64009a7dbf0b18433fd8b384e40a23a96d530ce
Luke Humphrey <luke.humphrey@ukaea.uk>
2024-02-19 11:08:30 +0000

    ✨ Add DesignEvaluator abstract base class

---
+++ src/sledo/design_evaluatior.py
@@ -0,0 +1,31 @@
+"""
+DesignEvaluator abstract base class for SLEDO.
+
+(c) Copyright UKAEA 2024.
+"""
+
+from abc import ABC, abstractmethod
+
+
+class DesignEvaluator(ABC):
+    """Abstract base class for evaluating a design. Must contain a method which
+    takes in the parameters describing a design and returns the
+    performance metrics for that design. Additional methods may be implemented
+    by a given subclass to perform the design evaluation procedure as required.
+    """
+
+    @abstractmethod
+    def evaluate_design(self, parameters: dict) -> dict:
+        """Evaluate a design and return performance metrics.
+
+        Parameters
+        ----------
+        parameters : dict
+            Dictionary of parameters describing the design to be evaluated.
+
+        Returns
+        -------
+        metrics: dict
+            Dictionary of metrics describing the design's performance as
+            evaluated.
+        """
