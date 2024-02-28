d0de31dc042ceccbed18507a8f4c1f36b8aa6800
Luke Humphrey <luke.humphrey@ukaea.uk>
2024-02-28 13:31:00 +0000

    ✅ Add TestMooseHerderDesignEvaluator.
    Add TestMooseHerderDesignEvaluator test class with placeholder tests. Also make minor formatting changes to the file.

--- tests/test_design_evaluator.py
+++ tests/test_design_evaluator.py
@@ -3,12 +3,21 @@ Tests for the SLEDO DesignEvaluator abstract base class and its subclasses.
 
 (c) Copyright UKAEA 2024.
 """
+import pytest
+
 from sledo.design_evaluator import (
     TestFunctionDesignEvaluator,
     MooseHerderDesignEvaluator,
 )
+from sledo.paths import SLEDO_ROOT
 
-import pytest
+TEST_INPUT_FILE = SLEDO_ROOT / "tests" / "test_data" / "input.i"
+
+
+@pytest.fixture(scope="session")
+def tmp_data_dir(tmp_path_factory):
+    tmp_data_dir = tmp_path_factory.mktemp("tmp_data_dir")
+    return tmp_data_dir
 
 
 class TestTestFunctionDesignEvaluator:
@@ -30,3 +39,22 @@ class TestTestFunctionDesignEvaluator:
         result = self.design_evaluator.evaluate_design(test_parameters)
         assert result == expected_result
 
+
+class TestMooseHerderDesignEvaluator:
+    """Tests for MooseHerderDesignEvaluator."""
+
+    @pytest.fixture(autouse=True)
+    def setup_method(self, tmp_data_dir):
+        metrics = ["temperature"]
+        self.design_evaluator = MooseHerderDesignEvaluator(
+            metrics,
+            base_input_file=TEST_INPUT_FILE,
+            working_dir=tmp_data_dir,
+        )
+
+    def test_has_required_methods(self):
+        assert hasattr(self.design_evaluator, "metrics")
+        assert hasattr(self.design_evaluator, "evaluate_design")
+
+    def test_evaluate_design(self):
+        pass
